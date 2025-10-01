03ecd9dc9bd93bf55505748c7c50bcbd63de61e6
Faisal Badshah <faisalbadshah46@gmail.com>
2025-10-01 17:15:20 +0530

    removed jsx from contact.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
-import Contact from "./pages/Contact.jsx";
+import Contact from "./pages/Contact";
 function App() {
   return (
     <Router>
