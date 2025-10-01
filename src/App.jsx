29c9581aa4b1d4e6a7d659c1cbf57e2bbc0b6f1b
Faisal Badshah <faisalbadshah46@gmail.com>
2025-10-01 17:30:20 +0530

    just fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-
-import Home from "@/pages/Home";
-import About from "@/pages/About";
-import Services from "@/pages/Services";
-import Contact from "@/pages/Contact";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
 function App() {
   return (
     <Router>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,9 +10,4 @@ export default defineConfig({
       },
     }),
   ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'), // <— this line allows '@/pages/Contact' imports
-    },
-  },
 })
